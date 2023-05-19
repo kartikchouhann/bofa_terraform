57f28d42ab97f3dc231792e0c668d276325d96c9
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2023-05-19 20:21:31 +0530

    Create ad.tf

---
+++ ad.tf
@@ -0,0 +1,5 @@
+azure_active_directory_role_based_access_control {
+      managed                = true
+      tenant_id              = "8587af23-dde8-4a87-a385-0d4080fa6df9"
+      azure_rbac_enabled     = true
+    }
